9223addf26a325294012cf9acd31714c6f9ae864
J0SE-ESC0BAR <josealfonsoescobarmejia@gmail.com>
2025-10-10 12:00:06 -0600

    Actualiza la ruta del controlador predeterminado a "Home" en la configuración de enrutamiento

--- WebApp.UI/Program.cs
+++ WebApp.UI/Program.cs
@@ -121,7 +121,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 // Crear roles por defecto
